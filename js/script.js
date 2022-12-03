731c139e50130161b08836c65bde19f87907914e
Marco 9726 <marcosarra9@gmail.com>
2022-12-03 16:15:51 +0100

    inizio struutura con ciclo for

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//1 - creare array lista della spesa
+const listaSpesa = ['Pane', 'Latte','Uova','Panettone','Pandoro','Torrone','Vino','Prosecco']
+// mi collego all'elemto html 
+const contSpesa = document.getElementById('lista');
+// creo variabile con contenuto della lista (li )  
+let element = "";
+// ciclo for 
+for (i=0; i<listaSpesa.length; i++){
+    // creo variabile contatore dei vari item della spesa 
+    let item = listaSpesa[i];
+    console.log(item);
+    // creo nella variabile degli li gli li stessi con struttura html e inserisco al loro interno la variabile item 
+    element += `<li class="list-item">${item}</li>`
+    contSpesa.innerHTML =element;
+}
--- readme.txt
+++ readme.txt
@@ -1,1 +1,4 @@
 Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+//1 - creare array lista della spesa X
+//2 - dare un id di riferimento nell'HTML nel quale andare a stampare la lista X
+//3 - creare un ciclo while
