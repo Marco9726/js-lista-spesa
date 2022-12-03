40b88403a5b0c6c5081169247552fa39b12155ab
Marco 9726 <marcosarra9@gmail.com>
2022-12-03 19:46:36 +0100

    stiliz. con css

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@
 const listaSpesa = ['Pane', 'Latte','Uova','Panettone','Pandoro','Torrone','Vino','Prosecco']
 // mi collego all'elemto html 
 const contSpesa = document.getElementById('lista');
-
+// creo contatore 
 let i = 0;
+let check = `<input type="checkbox" name="check">`;
 while ( i<listaSpesa.length){
     // creo variabile contatore dei vari item della spesa 
     let item = listaSpesa[i];
     // creo gli li 
     let listItem = document.createElement('li')
-    // inserisco all'interno degli li gli item 
-    listItem.innerHTML = `${item}`
+    // inserisco all'interno degli li gli item  
+    listItem.innerHTML = `${item}` + check
     // inserisco gli li nell'html 
     contSpesa.append(listItem)
     // aumento valore di i di 1 e faccio ripartire una nuova iterzazione 
     i++
 }
+
+
+// PERCHè GLI INPUT NON SONO INTERAGIBILI?? 
